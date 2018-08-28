65a3f4f774e39f2effa60b16ba86a7720a0ff1a2
Marcus Thelin <marcusthelin97@gmail.com>
2018-08-29 01:10:52 +0200

    added port

--- app.js
+++ app.js
@@ -2,8 +2,9 @@
 const express = require('express');
 // Create a new web server
 const app = express();
+const port = process.env.PORT || 3000;
 // Tell the web server to serve files
 // from the www folder
 app.use(express.static('www'));
 // Start the web server on port 3000
-app.listen(3000,() => console.log('Listening on port 3000'));
+app.listen(port, () => console.log('Listening on port 3000'));
