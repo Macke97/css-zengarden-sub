685d31dc258e340e27c32f14bcde1afa1947925f
Marcus Thelin <marcusthelin97@gmail.com>
2018-08-29 01:13:58 +0200

    added sendfile

--- app.js
+++ app.js
@@ -6,5 +6,9 @@ const port = process.env.PORT || 3000;
 // Tell the web server to serve files
 // from the www folder
 app.use(express.static(__dirname + '/www'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/www/index.html');
+});
 // Start the web server on port 3000
 app.listen(port, () => console.log('Listening on port 3000'));
