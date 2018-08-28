a02fb184e4a28ab1bb61e7a96e90d004322b0ca7
Marcus Thelin <marcusthelin97@gmail.com>
2018-08-29 01:12:06 +0200

    added dirname

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 // Tell the web server to serve files
 // from the www folder
-app.use(express.static('www'));
+app.use(express.static(__dirname + '/www'));
 // Start the web server on port 3000
 app.listen(port, () => console.log('Listening on port 3000'));
